1c0d05a12e51c569cc04348d86f4070245e9f89f
DineshBSTeam <102948004+DineshBSTeam@users.noreply.github.com>
2024-07-08 22:17:34 +0530

    Update Constants.kt
    review credentials

--- BsLibrary/src/main/java/com/lib/bslibrary/internal/helper/Constants.kt
+++ BsLibrary/src/main/java/com/lib/bslibrary/internal/helper/Constants.kt
@@ -2,21 +2,13 @@ package com.lib.bslibrary.internal.helper
 
 class Constants {
     companion object {
-        // with agentsdk
-        const val BASE_URL = "https://agentsdkapi.banksathi.com/api/"
-        const val advisorCode = "30201920"
-        const val clientSecret = "#u4yQBEJ2gnP4Ju0n?a=tW1twHBQSJ"
-        const val agentMobile = "9414468070"
-        const val agentName = "test"
-        const val agentEmail = "test@bs.com"
-
-        /* // with tryagentsdk
+     // with tryagentsdk
          const val BASE_URL = "https://tryagentsdkapi.banksathi.com/api/"
          const val advisorCode = "30201920"
          const val clientSecret = "DEV4uguysSDK4Agent?m=maRch724"
          const val agentMobile = "7892507675"
          const val agentName = "Test"
-         const val agentEmail = "test@bn.com"*/
+         const val agentEmail = "test@bn.com"
 
     }
 }
